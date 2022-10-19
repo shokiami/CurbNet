02d4437016c958fe3a6c1bdeb96ea02f00ffc86c
shokiami <kiami.sho@gmail.com>
2022-10-18 23:53:19 -0700

    fixed coord calculation and implemented load_label_data

--- coord_comparison_tool/coord_comparison.js
+++ coord_comparison_tool/coord_comparison.js
@@ -1,5 +1,5 @@
 var panorama;
-var label_data;
+var label_data = [];
 
 function main() {
   const myForm = document.getElementById("myForm");
@@ -12,8 +12,8 @@ function main() {
 
     reader.onload = function (e) {
       const text = e.target.result;
-      label_data = d3.csvParse(text);
-      on_csv_selected(label_data);
+      let csv_label_data = d3.csvParse(text);
+      load_label_data(csv_label_data);
     };
 
     reader.readAsText(iMathut);
@@ -25,55 +25,41 @@ function main() {
       pano: "EJ9gACh3lHF56-vu8I437g",
     }
   );
-
-  // window.setTimeout(() => {
-  //   panorama.setPov({
-  //     pitch: 0,
-  //     heading: 0,
-  //   });
-  // }, 1000);
-  on_csv_selected();
+  // panorama.setPov({
+  //   pitch: 0,
+  //   heading: 0,
+  // });
 }
 
-function on_csv_selected(label_data) {
-  let label_metadata = {
-    "label_id": "2176",
-    "gsv_panorama_id": "ox5UQtVmh6ldzaNUldygfg",
-    "label_type_id": "1",
-    "agree_count": "1",
-    "disagree_count": "0",
-    "notsure_count": "0",
-    "image_width": "16384.0",
-    "image_height": "8192.0",
-    "sv_image_x": "6002",
-    "sv_image_y": "-364",
-    "canvas_width": "720",
-    "canvas_height": "480",
-    "canvas_x": "536",
-    "canvas_y": "166",
-    "zoom": "2",
-    "heading": "148.9375",
-    "pitch": "-16.214284896850586",
-    "photographer_heading": "199.01522827148438",
-    "photographer_pitch": "0.29087066650390625"
+function load_label_data(csv_label_data) {
+  for (let label_metadata of csv_label_data) {
+    let [calculated_x, calculated_y] = calculate_coords(label_metadata);
+    label_data.push({
+      "label_id": label_metadata["label_id"],
+      "panorama_id": label_metadata["gsv_panorama_id"],
+      "calculated_x": calculated_x,
+      "calculated_y": calculated_y,
+      "actual_x": null,
+      "actual_y": null
+    });
   }
-  console.log(get_sv_coords(label_metadata));
+  console.log(label_data);
 }
 
-function get_sv_coords(label_metadata) {
-  let image_width = label_metadata["image_width"];
-  let image_height = label_metadata["image_height"];
-  let canvas_width = label_metadata["canvas_width"];
-  let canvas_height = label_metadata["canvas_height"];
-  let canvas_x = label_metadata["canvas_x"];
-  let canvas_y = label_metadata["canvas_y"];
-  let zoom = label_metadata["zoom"];
-  let heading = label_metadata["heading"];
-  let pitch = label_metadata["pitch"];
-  let photographer_heading = label_metadata["photographer_heading"];
-  let photographer_pitch = label_metadata["photographer_pitch"];
-  let h0 = heading * Math.pi / 180.0;
-  let p0 = pitch * Math.pi / 180.0;
+function calculate_coords(label_metadata) {
+  let image_width = parseInt(label_metadata["image_width"]);
+  let image_height = parseInt(label_metadata["image_height"]);
+  let canvas_width = parseInt(label_metadata["canvas_width"]);
+  let canvas_height = parseInt(label_metadata["canvas_height"]);
+  let canvas_x = parseInt(label_metadata["canvas_x"]);
+  let canvas_y = parseInt(label_metadata["canvas_y"]);
+  let zoom = parseFloat(label_metadata["zoom"]);
+  let heading = parseFloat(label_metadata["heading"]);
+  let pitch = parseFloat(label_metadata["pitch"]);
+  let photographer_heading = parseFloat(label_metadata["photographer_heading"]);
+  let photographer_pitch = parseFloat(label_metadata["photographer_pitch"]);
+  let h0 = heading * Math.PI / 180.0;
+  let p0 = pitch * Math.PI / 180.0;
   let fov = 126.5 - (zoom * 36.75);
   if (zoom > 2) {
     fov = 195.93 / (1.92 ** zoom);
@@ -99,9 +85,9 @@ function get_sv_coords(label_metadata) {
   let p = Math.asin(z / R);
   heading = h * 180.0 / Math.PI;
   pitch = p * 180.0 / Math.PI;
-  let horizontal_scale = 2 * Math.pi / image_width;
+  let horizontal_scale = 2 * Math.PI / image_width;
   let amplitude = photographer_pitch * image_height / 180;
-  let original_x = Math.round((heading - photographer_heading) / 180 * image_width / 2 + image_width / 2) % image_width;
+  let original_x = (Math.round((heading - photographer_heading) / 180 * image_width / 2 + image_width / 2) + image_width) % image_width;
   let original_y = Math.round(image_height / 2 + amplitude * Math.cos(horizontal_scale * original_x));
   let cosine_slope = amplitude * -Math.sin(horizontal_scale * original_x) * horizontal_scale;
   let offset_x = 0;
@@ -121,5 +107,5 @@ function get_sv_coords(label_metadata) {
   let offset_vec_scalar = -pitch / 180 * image_height;
   let finalized_offset_x = normalized_offset_x * offset_vec_scalar;
   let finalized_offset_y = normalized_offset_y * offset_vec_scalar;
-  return Math.round(original_x + finalized_offset_x), Math.round(original_y + finalized_offset_y);
+  return [Math.round(original_x + finalized_offset_x), Math.round(original_y + finalized_offset_y)];
 }
