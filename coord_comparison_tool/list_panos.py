f5d6d9343c6106ef6a27bfc97c5ba3f9dd2940b2
shokiami <kiami.sho@gmail.com>
2022-11-16 00:07:26 -0800

    moved dirname to const

--- coord_comparison_tool/list_panos.py
+++ coord_comparison_tool/list_panos.py
@@ -2,5 +2,7 @@ import glob
 import os
 import pandas as pd
 
-panos_df = pd.DataFrame(os.path.basename(path) for path in glob.glob("../panos/*.jpg"))
+PANOS_DIR = "../panos"
+
+panos_df = pd.DataFrame(os.path.basename(path) for path in glob.glob(PANOS_DIR + "/*.jpg"))
 panos_df.to_csv("../panos_list.csv", header=False, index=False)
